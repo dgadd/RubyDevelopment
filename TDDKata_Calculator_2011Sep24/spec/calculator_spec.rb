e509a7c95a1427e5fc11e3150e5c5c0197491b33
David Gadd <gaddzeit@yahoo.ca>
2011-09-25 23:11:47 -0700

    KATA-1 Test 4: any length input returns sum

--- TDDKata_Calculator_2011Sep24/spec/calculator_spec.rb
+++ TDDKata_Calculator_2011Sep24/spec/calculator_spec.rb
@@ -15,10 +15,23 @@ describe "My Calculator's add method'" do
     result.should equal 3
   end
 
-  it "should return sum for a two-length with comma string input" do
+  it "should return sum for a two-length with comma-separated input" do
     sut = Calculator.new
     result = sut.add("3,5")
     result.should equal 8
+  end
+
+  it "should return sum for a any-length with comma-delimited input" do
+    how_many = rand(999)
+    numbers_to_add = ''
+    expected = 0
+    (1..how_many).each do |i|
+      numbers_to_add += i.to_s + ','
+      expected += i
+    end
+    sut = Calculator.new
+    result = sut.add(numbers_to_add)
+    result.should equal expected
   end
 
 end
