9888181c906833241560527bf61187c796f1f72b
David Gadd <gaddzeit@yahoo.ca>
2011-09-27 19:13:04 -0700

    KATA-2 Test 3: two-length input returns sum

--- TDDKata_Calculator_2011Sep27/calculator.rb
+++ TDDKata_Calculator_2011Sep27/calculator.rb
@@ -1,6 +1,11 @@
 class Calculator
   def add(numbers_to_add)
+    return sum(numbers_to_add.split(',')) if numbers_to_add.include?(",")
     return numbers_to_add.to_i if numbers_to_add.length > 0
     return 0
+  end
+
+  def sum(numbers_array)
+    return numbers_array.inject(0) { |total, number_string| total += number_string.to_i }
   end
 end
--- TDDKata_Calculator_2011Sep27/spec/calculator_spec.rb
+++ TDDKata_Calculator_2011Sep27/spec/calculator_spec.rb
@@ -14,4 +14,10 @@ describe "Calculator Add method" do
     result.should equal 4
   end
 
+  it "should return sum for a two-length comma-delimited input param" do
+    sut = Calculator.new
+    result = sut.add("4,5")
+    result.should equal 9
+  end
+
 end
