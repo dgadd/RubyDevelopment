0a2eb5675cfe96e4b1e25f25d8ab63cfd23ea779
David Gadd <gaddzeit@yahoo.ca>
2011-10-01 10:57:23 -0700

    KATA-4 Created Rails project

---
+++ TDDKata_Rails_2011Oct01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TDDKataRails2011Oct01::Application.config.secret_token = 'bacce78d307459e827e5b89de8e7aebe382d9f9db3c42b2d709c110d7be466df9d018a273a35a1ac7ec6bbc0d2025f674973added23234d9183f575a3dbb1042'
---
+++ TDDKata_Rails_2011Oct01/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TDDKataRails2011Oct01::Application.config.session_store :cookie_store, :key => '_TDDKata_Rails_2011Oct01_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TDDKataRails2011Oct01::Application.config.session_store :active_record_store
