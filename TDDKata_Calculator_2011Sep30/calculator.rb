c7fbb59858048205e5d68b374e1f4fffc6cd0433
David Gadd <gaddzeit@yahoo.ca>
2011-09-30 18:27:50 -0700

    KATA-3 Test 1: 1-lenght input returns equivalent

--- TDDKata_Calculator_2011Sep30/calculator.rb
+++ TDDKata_Calculator_2011Sep30/calculator.rb
@@ -1,6 +1,7 @@
 class Calculator
   # To change this template use File | Settings | File Templates.
   def add(numbers_to_add)
+    return numbers_to_add.to_i if numbers_to_add.length > 0
     0
   end
 end
--- TDDKata_Calculator_2011Sep30/spec/calculator_spec.rb
+++ TDDKata_Calculator_2011Sep30/spec/calculator_spec.rb
@@ -8,4 +8,11 @@ describe "Calculator Adds" do
     result = sut.add ""
     result.should equal 0
   end
+
+  it "should return equivalent number for a single-length input" do
+    sut = Calculator.new
+    result = sut.add "3"
+    result.should equal 3
+  end
+
 end
