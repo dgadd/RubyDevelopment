662fbf822aff4b7865fc0546fdfa8c8eafdb5765
David Gadd <gaddzeit@yahoo.ca>
2011-09-30 18:24:36 -0700

    KATA-3 Ruby Calculator kata project created

---
+++ TDDKata_Calculator_2011Sep30/calculator.rb
@@ -0,0 +1,3 @@
+class Calculator
+  # To change this template use File | Settings | File Templates.
+end
---
+++ TDDKata_Calculator_2011Sep30/spec/calculator_spec.rb
@@ -0,0 +1,11 @@
+require "rspec"
+require "../calculator"
+
+describe "Calculator Adds" do
+
+  it "should return 0 for a zero-length input" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
