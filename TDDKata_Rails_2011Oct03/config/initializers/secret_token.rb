fba2007428d27475cd9eb2e18c110a51736ebcd1
David Gadd <gaddzeit@yahoo.ca>
2011-10-03 22:27:14 -0700

    KATA-5 Set up rails app with application.helper.erb

---
+++ TDDKata_Rails_2011Oct03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TDDKataRails2011Oct03::Application.config.secret_token = '9b2dc233228fb6f8cb80d2092143e4043ddb4523df0fcef8604f035f0e2911a38543cf7df997fa890f31659deb368d60c592444e3dbcbd573aa06aefb4585799'
