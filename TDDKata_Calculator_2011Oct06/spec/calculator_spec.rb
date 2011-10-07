d5c31724793e4166cc6fcf79f4cb5bf7dbd016bb
David Gadd <gaddzeit@yahoo.ca>
2011-10-06 19:04:34 -0700

    KATA-6 Test 4: any length string should return sum

--- TDDKata_Calculator_2011Oct06/spec/calculator_spec.rb
+++ TDDKata_Calculator_2011Oct06/spec/calculator_spec.rb
@@ -15,10 +15,23 @@ describe "Calculator" do
       result.should equal 4
     end
 
-    it("should return sum for comma-delimited string input") do
+    it("should return sum for 2 comma-delimited string input") do
       sut = Calculator.new
       result = sut.add "4,5"
       result.should equal 9
+    end
+
+    it("should return sum for any length comma-delimited string input") do
+      how_many = rand(999)
+      numbers_to_add = ""
+      expected = 0
+      (0..how_many).each do |i|
+        numbers_to_add += i.to_s + ","
+        expected += i
+      end
+      sut = Calculator.new
+      result = sut.add numbers_to_add
+      result.should equal expected
     end
 
   end
