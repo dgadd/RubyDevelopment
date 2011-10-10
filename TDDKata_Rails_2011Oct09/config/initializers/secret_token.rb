e7bec9dca9d29d7bbb4c56e7edf61c9cd63e811c
David Gadd <gaddzeit@yahoo.ca>
2011-10-09 17:22:51 -0700

    KATA-8 Set up Rails with RSpec

---
+++ TDDKata_Rails_2011Oct09/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TDDKataRails2011Oct09::Application.config.secret_token = '06d750fad484b45b2e98941fe0eec6ea99aedae6e35b2c157fc4e0dae86ed4ffb9803c2b266b001338af3692fc8bda09ac9b4329003b742249f8d4204fa9fd8f'
---
+++ TDDKata_Rails_2011Oct09/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TDDKataRails2011Oct09::Application.config.session_store :cookie_store, :key => '_TDDKata_Rails_2011Oct09_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TDDKataRails2011Oct09::Application.config.session_store :active_record_store
