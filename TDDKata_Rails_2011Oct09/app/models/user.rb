f526fa3cfed7ec749064212d0925d056ed052b79
David Gadd <gaddzeit@yahoo.ca>
2011-10-12 09:00:56 -0700

    KATA-8 Added export of rspec tests in HTML format

--- TDDKata_Rails_2011Oct09/app/models/user.rb
+++ TDDKata_Rails_2011Oct09/app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+
 end
