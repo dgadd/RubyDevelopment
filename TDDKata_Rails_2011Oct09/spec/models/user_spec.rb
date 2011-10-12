ed394e349e651c374359e7d9574a08e3c46ca596
David Gadd <gaddzeit@yahoo.ca>
2011-10-12 08:57:28 -0700

    KATA-8 Added tests to verify duplicate emails are rejected, with case-sensitivity

--- TDDKata_Rails_2011Oct09/spec/models/user_spec.rb
+++ TDDKata_Rails_2011Oct09/spec/models/user_spec.rb
@@ -1,3 +1,6 @@
+# These tests are being created based on the instructions in the Ruby on Rails Tutorial site, located here:
+# http://ruby.railstutorial.org/chapters/modeling-and-viewing-users-one
+
 require 'spec_helper'
 
 describe User do
@@ -40,6 +43,20 @@ describe User do
       invalid_email_user = User.new(@attr.merge(:email => address))
       invalid_email_user.should_not be_valid
     end
+  end
+
+  it "should reject duplicate email addresses" do
+    # Put a user with given email address into the database.
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+
+  it "should reject email addresses identical up to case" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
   end
 
 end
