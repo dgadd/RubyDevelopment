1fddeb627abe08bfda01afd70621695a191b8a26
David Gadd <gaddzeit@yahoo.ca>
2011-10-16 10:04:55 -0700

    KATA-9 Rails project initial setup complete

---
+++ TDDKata_Rails_2011Oct16/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TDDKataRails2011Oct16::Application.config.secret_token = '47601547c6f9a885bad54a2572960b0f50b48a1c45063ce636c482ea04d9ae15871645c1576de10329e7bdfee48e82dc6360019fbbdaad02fc9a1c6e75e2b21c'
