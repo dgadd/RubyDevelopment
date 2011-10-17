c98f735cf1ca0a2b159e962749c313d7bd723e0c
David Gadd <gaddzeit@yahoo.ca>
2011-10-16 18:09:52 -0700

    KATA-9 Added all *_path links and layout_links spec tests to verify them

--- TDDKata_Rails_2011Oct16/config/routes.rb
+++ TDDKata_Rails_2011Oct16/config/routes.rb
@@ -2,6 +2,7 @@ TDDKataRails2011Oct16::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
