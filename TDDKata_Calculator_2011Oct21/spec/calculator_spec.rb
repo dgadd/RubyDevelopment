2f7772b54a8f261a0c93dee1fc0b887564d3a6bf
David Gadd <gaddzeit@yahoo.ca>
2011-10-21 18:45:18 -0700

    KATA-10 Set up rspec and empty test

---
+++ TDDKata_Calculator_2011Oct21/spec/calculator_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+
+describe "Add method" do
+
+  it "should return 0 from an empty string" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
