3752a2aa17679873f2c35396f3dd85dbc1439bbf
David Gadd <gaddzeit@yahoo.ca>
2011-10-22 11:10:44 -0700

    KATA-10 Updated the kata with (copy/paste of) rspec/specify tests

--- TDDKata_Calculator_2011Oct21/calculator.rb
+++ TDDKata_Calculator_2011Oct21/calculator.rb
@@ -1,6 +1,24 @@
 class Calculator
-  # To change this template use File | Settings | File Templates.
-  def add(numbersToAdd)
+
+  def initialize(default_numbers_to_add = "", fp_value = 0.0)
+    @default_numbers_to_add = default_numbers_to_add
+    @fp_value = fp_value
+  end
+
+  def add(numbers_to_add)
+    numbers_to_add = @default_numbers_to_add if @default_numbers_to_add.length > 0
     0
+  end
+
+  def default_array()
+    @default_numbers_to_add.split(",")
+  end
+
+  def add_to_default(new_number)
+    @default_numbers_to_add += "," + new_number.to_s
+  end
+
+  def fp_value()
+    @fp_value
   end
 end
