e88ecf1d63d615b9d755e851720a5bae5e9714e0
David Gadd <gaddzeit@yahoo.ca>
2011-11-12 15:17:16 -0800

    KATA-11 Pages controller tests passed with title verification tests

--- TDDKata_Rails_2011Nov12/app/controllers/pages_controller.rb
+++ TDDKata_Rails_2011Nov12/app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def help
+    @title = "Help"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def about
+    @title = "Abouts"
   end
 
 end
